6259457c6497115d89a1f59ceaf1c5762756d7c4
Loren Hicks <lorenhicks@gmail.com>
2023-12-18 09:21:40 -0500

    Initializing Entities, Getters and Setters

--- src/main/java/com/lhdevserver/conferencecheckin/model/Attendee.java
+++ src/main/java/com/lhdevserver/conferencecheckin/model/Attendee.java
@@ -1,4 +1,42 @@
 package com.lhdevserver.conferencecheckin.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Attendee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String company;
+
+    public Attendee() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
 }
