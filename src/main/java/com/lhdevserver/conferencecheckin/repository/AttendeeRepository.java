b1aed625e1cb4b9517aa798e6fe7ed0d992dd41a
Loren Hicks <lorenhicks@gmail.com>
2023-12-18 22:44:30 -0500

    Initializing AttendeeRepository

--- src/main/java/com/lhdevserver/conferencecheckin/repository/AttendeeRepository.java
+++ src/main/java/com/lhdevserver/conferencecheckin/repository/AttendeeRepository.java
@@ -1,4 +1,9 @@
 package com.lhdevserver.conferencecheckin.repository;
 
-public interface AttendeeRepository {
+import com.lhdevserver.conferencecheckin.model.Attendee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AttendeeRepository extends JpaRepository<Attendee,Integer> {
 }
